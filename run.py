3ff96bf89abfd772420020d44ddb18a4131463ed
Luke Maycock <maycock.luke@gmail.com>
2024-02-25 22:29:20 +0000

    Updating validate_data function with docstring from walkthrough

--- run.py
+++ run.py
@@ -34,6 +34,11 @@ def get_sales_data():
 
 
 def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
